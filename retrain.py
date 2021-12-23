e8ad72377ab6be02a3081c6de765c0dd56bdf790
Dain Kim <dannykm@naver.com>
2021-12-24 03:24:55 +0900

    Add files via upload

--- retrain.py
+++ retrain.py
@@ -39,7 +39,7 @@ with DAG(
     # bash 커맨드로 echo hello 를 실행합니다.
     install_requirements = BashOperator(
         task_id="install_requirements",
-        bash_command="pip install --cache-dir=$HOME/new/tmp/dir/ -r /opt/airflow/dags/requirements.txt",
+        bash_command="pip install --no-cache-dir -r /opt/airflow/dags/requirements.txt",
         owner="dain",  # 이 작업의 오너입니다. 보통 작업을 담당하는 사람 이름을 넣습니다.
         dag=dag
     )

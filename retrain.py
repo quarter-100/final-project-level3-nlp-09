7162be77d35a5be0a1ac452193e45547db775066
Dain Kim <dannykm@naver.com>
2021-12-25 21:51:50 +0900

    Add files via upload

--- retrain.py
+++ retrain.py
@@ -40,7 +40,7 @@ with DAG(
     install_requirements = BashOperator(
         task_id="install_requirements",
         #bash_command="pip install torch",
-        bash_command="pip install -r /opt/airflow/dags/requirements.txt",
+        bash_command="pip install -r ./final-project-level3-nlp-09/requirements.txt",
         owner="dain",  # 이 작업의 오너입니다. 보통 작업을 담당하는 사람 이름을 넣습니다.
         dag=dag
     )
@@ -62,22 +62,22 @@ with DAG(
     )
     train_task = BashOperator(
         task_id='train_yes_or_no',
-        bash_command='python /opt/airflow/dags/train.py',
+        bash_command='python ./final-project-level3-nlp-09/train.py',
         owner="dain",
         dag=dag
     )
     """
     inference_task1 = BashOperator(
         task_id='inference_best_model',
-        bash_command='python /opt/airflow/dags/inference_best.py',
+        bash_command='python ./final-project-level3-nlp-09/inference_best.py',
         owner="dain",
         dag=dag
     )
 
     retrain_model = BashOperator(
         task_id='retrain_model',
-        #bash_command='echo "retrain!!"',
-        bash_command='python /opt/airflow/dags/train.py',
+        bash_command='echo "retrain!!"',
+        #bash_command='python ./final-project-level3-nlp-09/train.py',
         owner="dain",
         dag=dag
     )
@@ -85,7 +85,7 @@ with DAG(
 
     inference_task2 = BashOperator(
         task_id='inference_retrained_model',
-        bash_command='python /opt/airflow/dags/inference_new.py',
+        bash_command='python ./final-project-level3-nlp-09/inference_new.py',
         owner="dain",
         dag=dag
     )
@@ -101,7 +101,7 @@ with DAG(
 
     upload_retrained_model = BashOperator(
         task_id='upload_retrained_model',
-        bash_command='python /opt/airflow/dags/upload_model.py',
+        bash_command='python ./final-project-level3-nlp-09/upload_model.py',
         owner="dain",
         dag=dag
     )
